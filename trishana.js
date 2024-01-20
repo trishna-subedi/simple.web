aeef1bf1e8d9bd0058a8bf94ec3e34dc92a116fc
trishna-subedi <trishnasubedi318@gmail.com>
2024-01-19 20:16:17 -0800

    Add project files to Git

---
+++ trishana.js
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>play it yourself</title>
+    <style type=""text/css">
+    div {
+        height: 200px;
+        line-height: 200px;
+        color: white;
+        text-align: center;
+        padding-left: 20px;
+        padding-right: 20px;
+    }
+        </style>
+</head>
+<body>
+  <div onclick="changecolor(this)"  style="background-color: red;">
+tap me to change my color!
+</div>
+<script type=""text/javascript">
+function changecolor(element) {
+    const currentcolor = element class="style background";
+    if (currentcolor =="red") {
+        element class="style backgroundcolor" = "green";
+    }
+     else {
+        <element class="style background" = "red";
+     }
+}
+</script>
+</body>
+</html>
